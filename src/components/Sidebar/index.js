28632c1811dcfa88e3ca635b4a732b15740f0709
AronaLeaga <102854382+AronaLeaga@users.noreply.github.com>
2022-09-05 20:25:32 +1200

    nav links to linked in

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,6 +4,7 @@ import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin, faYoutube } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
     <div className='nav-bar' >
@@ -26,6 +27,32 @@ const Sidebar = () => (
       </NavLink>
 
     </nav>
+    <ul>
+    <li>
+        <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/aronaleagamini/'>
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+        </a>
+    </li>
+
+    <li>
+        <a target="_blank" rel='noreferrer' href='https://github.com/AronaLeaga'>
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+        </a>
+    </li>
+
+    <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/aronaleagamini/"
+        >
+          <FontAwesomeIcon icon={faYoutube} color="#4d4d4e" />
+        </a>
+      </li>
+
+    </ul>
+
+
     </div>
 )
 
